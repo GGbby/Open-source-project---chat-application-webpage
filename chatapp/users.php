6f811dea4e7852241eaa0ed988756050672f754c
GGbby <brian1375@gmail.com>
2022-06-12 18:02:29 +0800

    Finish setting the header.php

--- chatapp/users.php
+++ chatapp/users.php
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
+<?php   
+    session_start();
+    if (!sset($_SESSION['unique_id'])) {
+        //header("loaction: ../login.php");
+    }
+?>
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-</head>
+<?php   include_once "header.php"; ?>
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
+                <?php
+                    session_start();
+                ?>
                 <div class="content">
                     <img src="img.jpg" alt="">
                     <div class="details">
