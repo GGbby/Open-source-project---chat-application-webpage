228dfb872e9c15999ec86ded10df9a86446e2e63
Tenderflash <66397269+Tenderrflash@users.noreply.github.com>
2022-06-13 08:46:22 +0800

    Ten final

--- chatapp/php/data.php
+++ chatapp/php/data.php
@@ -1,6 +1,6 @@
 <?php
     while($row = mysqli_fetch_assoc($sql)){
-
+        ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
         $output .= '<a href="chat.php?user_id='.$row['unique_id'].'">
                     <div class="content">
                     <img src="php/images/'. $row['img'] .'" alt="">
@@ -9,10 +9,11 @@
                     
                     </div>
                     </div>
-                    
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                    <div class="status-dot '.$offline.'"><i class="fas fa-circle"></i></div>
                     </a>';
     }
+    // <div class="status-dot"><i class="fas fa-circle"></i></div>
+
     // $result = $row2['msg'];
     // $result = "No message available";
     // (strlen($result) > 28) ? $msg = substr($result, 0, 28).'...' : $msg = $result;
